162e8fab321b47672e4dd307a4ee588d396dadf5
Axel <axel@sweetas.com>
2021-01-09 08:27:25 +0100

    rewrite to typescript

--- SolidBarnacle/src/index.ts
+++ SolidBarnacle/src/index.ts
@@ -1,27 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+import { app } from 'electron';
+import Main from './app';
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  win.loadFile('../index.html')
-}
-
-app.whenReady().then(createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+Main.main(app);
