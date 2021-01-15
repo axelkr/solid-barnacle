d36d4909707f3a3d1b17cd952f1cd94adb327385
axelkr <someone@gmail.com>
2021-01-15 06:54:43 +0100

    default test no longer applicatble

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,11 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
+/*
   it(`should have as title 'SolidBarnacle'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('SolidBarnacle');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('SolidBarnacle app is running!');
-  });
+*/
 });
