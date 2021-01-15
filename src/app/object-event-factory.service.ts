19308ad2293b65eb48220f4a179f4f8715a94d07
axelkr <someone@gmail.com>
2021-01-15 13:06:00 +0100

    last linting errors for now

--- src/app/object-event-factory.service.ts
+++ src/app/object-event-factory.service.ts
@@ -24,8 +24,8 @@ export class ObjectEventFactoryService {
   }
 
   private createUUID(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) { // eslint-disable-line
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);// eslint-disable-line
       return v.toString(16);
     });
   }
