065e61434b774f5d38ad46e664423af68a704f1c
axelkr <someone@gmail.com>
2021-01-15 09:47:34 +0100

    creation will be delegated to model of course

--- src/app/task-add/task-add.component.spec.ts
+++ src/app/task-add/task-add.component.spec.ts
@@ -2,13 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaskAddComponent } from './task-add.component';
 
+import { ModelTasksService } from '../model-tasks.service';
+
 describe('TaskAddComponent', () => {
   let component: TaskAddComponent;
   let fixture: ComponentFixture<TaskAddComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskAddComponent ]
+      declarations: [ TaskAddComponent,ModelTasksService ]
     })
     .compileComponents();
   });
--- src/app/task-add/task-add.component.ts
+++ src/app/task-add/task-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModelTasksService } from '../model-tasks.service';
 
 @Component({
   selector: 'app-task-add',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modelTasksService: ModelTasksService) { }
 
   ngOnInit(): void {
   }
