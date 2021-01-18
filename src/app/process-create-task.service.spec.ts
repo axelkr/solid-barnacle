8821db4b9a627bc56eb287b5f76ea92930321465
Axel <axel@sweetas.com>
2021-01-18 07:47:17 +0100

    feat: discard own / duplicate events

--- src/app/process-create-task.service.spec.ts
+++ src/app/process-create-task.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { ProcessCreateTaskService } from './process-create-task.service';
 
+import { Task } from './task';
+import { ObjectEvent } from './objectEvent';
+
 describe('ProcessCreateTaskService', () => {
   let service: ProcessCreateTaskService;
 
@@ -12,5 +15,21 @@ describe('ProcessCreateTaskService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should discard CreateTaskEvent if already created', () => {
+    const inputTasks: Task[] = [];
+    const aCreateTaskEvent: ObjectEvent = {
+      topic: 'aTopic',
+      time: new Date(),
+      id: 23,
+      eventType: service.objectEventTypeProcessing,
+      object: 'asda_asda_asdads',
+      objectType: 'objectType',
+      payload: new Map<string,string>([['name','name'],['state','state']])
+    };
+    const afterProcessingEventOnce = service.process(aCreateTaskEvent,inputTasks);
+    const afterProcessingEventTwice = service.process(aCreateTaskEvent,afterProcessingEventOnce);
+    expect(afterProcessingEventTwice.length).toEqual(inputTasks.length+1);
   });
 });
